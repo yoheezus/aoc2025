b53d589e27f995bdf770578e52a4e236352e9086
yohann <yohann@yohann.com>
2025-12-04 13:31:09 +0000

    Solution part 1 and 2

--- day1/main.go
+++ day1/main.go
@@ -52,47 +52,45 @@ func parseInstructions(instructions []string) []int {
 
 }
 
+func (w *Wheel) clickAdd(x int) {
+	w.clicks += x
+	if w.clicks < 0 {
+		w.clicks = 0
+	}
+}
+
 func (w *Wheel) Spin(instruction int) int {
+	var direction int
 	start := w.idx
-	local_clicks := 0
-	new_pos := start + instruction
-	if new_pos == 100 {
-		new_pos = 0
-	}
-	started_zero := start == 0
+	negative := instruction < 0
+	steps_left := int(math.Abs(float64(instruction)))
 
-	for new_pos > w.length-1 || new_pos < 0 {
-
-		if started_zero || new_pos == 0 {
-			started_zero = false
-		} else {
-			local_clicks += 1
-		}
-
-		if new_pos == 100 {
-			new_pos = 0
-		}
-
-		// Over Bounds
-		if new_pos > w.length-1 {
-			new_pos -= w.length
-
-		}
-		// Under Bounds
-		if new_pos < 0 {
-			new_pos += w.length // Addition because number is negative
-
-		}
-
+	if negative {
+		direction = -1
+	} else {
+		direction = 1
 	}
 
-	if new_pos == 0 {
+	for steps_left != 0 {
+		w.idx += direction
+		steps_left -= 1
+
+		if w.idx == w.length {
+			w.idx = 0
+		} else if w.idx < 0 {
+			w.idx = w.length - 1
+		}
+		if w.idx == 0 {
+			w.clicks += 1
+		}
+	}
+	fmt.Printf("Starting %d, instruction %d, ends at %d, clicks: %d on_zeroes:%d\n", start, instruction, w.idx, w.clicks, w.on_zero)
+
+	if w.idx == 0 {
+		w.clicks -= 1
 		w.on_zero += 1
 	}
 
-	// fmt.Printf("Starting %d, instruction %d, ends at %d, clicks: %d on_zeroes:%d\n", start, instruction, new_pos, local_clicks, w.on_zero)
-	w.idx = new_pos
-	w.clicks += local_clicks
 	return w.idx
 }
 
@@ -116,9 +114,53 @@ func main() {
 	}
 	instructions := parseInstructions(raw_instructions)
 	// instructions := []int{-68, -30, 48, -5, 60, -55, -1, -99, 14, -82}
-	// instructions := []int{200}
+	// instructions := []int{-2}
 	_ = DoSpins(instructions, &w)
 
 	fmt.Printf("Lands: %d, Passes: %d, answer: %d idx: %d\n", w.on_zero, w.clicks, w.on_zero+w.clicks, w.idx)
 
 }
+
+//  Method that I could not get working but I don't know why
+// func (w *Wheel) Spin(instruction int) int {
+// 	start := w.idx
+// 	// local_clicks := 0
+// 	new_pos := start + instruction
+// 	if new_pos == 100 {
+// 		new_pos = 0
+// 	}
+// 	// started_zero := start == 0
+
+// 	for new_pos > w.length-1 || new_pos < 0 { // While out of bounds
+// 		w.clickAdd(1)
+
+// 		if new_pos == 100 {
+// 			new_pos = 0
+// 		}
+
+// 		// Over Bounds
+// 		if new_pos > w.length-1 {
+// 			new_pos -= w.length
+
+// 		}
+// 		// Under Bounds
+// 		if new_pos < 0 {
+// 			new_pos += w.length // Addition because number is negative
+
+// 		}
+
+// 		fmt.Printf("Starting %d, instruction %d, ends at %d, clicks: %d on_zeroes:%d\n", start, instruction, new_pos, w.clicks, w.on_zero)
+// 	}
+
+// 	// if started_zero {
+// 	// 	w.clickAdd(-1)
+// 	// }
+
+// 	if new_pos == 0 {
+// 		w.on_zero += 1
+// 	}
+
+// 	w.idx = new_pos
+// 	// w.clicks += local_clicks
+// 	return w.idx
+// }
